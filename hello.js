1990c29fbfbe47a569582f3cdf6279184071b924
Jaishali <142702371+Jaishali@users.noreply.github.com>
2023-09-17 15:09:08 -0500

    testing changes
    Updated

--- hello.js
+++ hello.js
@@ -3,12 +3,118 @@ Welcome, new coder.
 Enter your code on line 7 to print 'Hello, World!' 
 to the console (the panel on the right)
 */
-console.log("Hello World");
+/*console.log("Hello World");
 console.log(Hello World);
 console.log(Hello World');
+let phrase = "JavaScript rocks!";
+console.log(phrase[phrase.length - 8]);
+let word = "Rutabaga";
+console.log(word,"has",word.length,"characters");
+cityName = "Vienna";
+stateName = "Virginia";
+location = cityName + ", " +  stateName;*/
 
-/*  
-Next, PLAY! 
+/*pet ='cat'
+console.log(pet+'s');
+pet = 's';*/
+
+/*console.log(location.length)*/
+/*let language = "JavaScript";
+language.replace('J', 'Q');
+language.slice(0,5);
+console.log(language);*/
+/*let language = 'JavaScript'
+console.log(language.slice(1,6));*/
+/*let org = "  The LaunchCode Foundation ";
+let trimmed = org.trim();
+
+console.log(trimmed);*/
+/*console.log('Launch\nCode');
+console.log('Launch/nCode');
+console.log('Launch', 'Code');
+console.log('Launch\tCode');
+console.log('Launch/tCode');*/
+/*let classes = ["science, computer, art"];
+console.log(classes.length);
+let teachers = ["Jones", "Willoughby", "Rhodes"];
+console.log(teachers.length);*/
+/*let charles = ['coder', 'Tech', 47, 23, 350];
+charles.sort();
+console.log(charles);*/
+/*let str =['LaunchCode students rock!'];
+console.log(str);*/
+/*let groceryBag = ['bananas', 'apples', 'edamame', 'chips', 'cucumbers', 'milk', 'cheese'];
+let selectedItems = [];
+
+selectedItems = groceryBag.slice(2, 5).sort();
+console.log(selectedItems);*/
+let school = [
+   ["science", "computer", "art"],
+   ["Jones", "Willoughby", "Rhodes"]
+
+];
+//let phrase ='Index';
+//console.log(phrase[0]);
+//let phrase = "JavaScript rocks!";
+//console.log(phrase[phrase.length - 8]);
+cityName = "Vienna";
+
+stateName = "Virginia";
+
+location = cityName + ", " + stateName
+
+console.log(location);
+console.log(location.length);
+
+let pet = 'cat'
+console.log(pet + 's');
+pet += 's';
+
+let language = 'JavaScript';
+//language.replace('J', 'Q');
+//language.slice(0,5);
+//console.log(language);
+//language.slice(1,6)
+console.log(language.slice(1,6));
+let org = "  The LaunchCode Foundation ";
+let trimmed = org.trim();
+
+console.log(trimmed);
+console.log('Launch\nCode');
+
+
+const classes = ["science, computer, art"];
+
+const teachers = ["Jones", "Willoughby", "Rhodes"];
+
+console.log(classes.length);
+console.log(teachers.length);
+str = 'LaunchCode students rock!'
+
+console.log(str.split(" "));
+const test = [
+   ["science", "computer", "art"],
+   ["Jones", "Willoughby", "Rhodes"]
+];
+//console.log(test[1][0]);
+//test.push([1][0], 'Peter');
+//test[1].splice(1,0,'testing');
+test[1].splice(1,0,'Holmes');
+
+
+//test.push([0][0])('Day-Scholar');
+console.log(test);
+let charles = ['coder', 'Tech', 47, 23, 350];
+charles.sort();
+console.log(charles);
+
+let groceryBag = ['bananas', 'apples', 'edamame', 'chips', 'cucumbers', 'milk', 'cheese'];
+let selectedItems = [];
+
+selectedItems = groceryBag.slice(2, 5).sort();
+console.log(selectedItems);
+
+/*Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
 
    1. Change the message that is printed.
